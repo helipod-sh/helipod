146495e7559602aa4c1c7c257331b0c03377b91c
dbjpanda <dbjpanda@users.noreply.github.com>
2026-07-03 10:27:00 +0530

    fix: update appName to 'stackbase' in shared.ts

--- docs/lib/shared.ts
+++ docs/lib/shared.ts
@@ -1,4 +1,4 @@
-export const appName = 'My App';
+export const appName = 'stackbase';
 export const docsRoute = '/docs';
 export const docsImageRoute = '/og/docs';
 export const docsContentRoute = '/llms.mdx/docs';
